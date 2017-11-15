a36d2d671a2a51c2b185bfc761107e40631ce919
blueash11 <33674398+blueash11@users.noreply.github.com>
2017-11-15 13:28:45 -0800

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
+# a class to define the movie contributes
 class Movie():
-    def __init__(self,title,trailer_youtube_url,poster_image_url):
-        self.title = title
-        self.trailer_youtube_url = trailer_youtube_url
-        self.poster_image_url = poster_image_url
+    # take movie title, trailer url and poster url as input, do initialization
+    def __init__(self, title, trailer_youtube_url, poster_image_url):
+        self.title = title   # movie title
+        self.trailer_youtube_url = trailer_youtube_url   # movie trailer url
+        self.poster_image_url = poster_image_url   # movie poster url
